62081ea54fd3395277c6da0bac910ce0ec3ac86e
IONIQ6000 <supes6000@proton.me>
2025-08-29 13:57:50 -0400

    fix

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -6,6 +6,16 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
   return (
     <NextThemesProvider
       attribute="class"
